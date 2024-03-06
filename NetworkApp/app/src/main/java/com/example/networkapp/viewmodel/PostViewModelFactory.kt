3b40a028a8b916e3e6c857c51f0d22b89ab00094
Ziklson <etothezik@gmail.com>
2024-03-06 15:22:33 +0300

    Initial commit

---
+++ NetworkApp/app/src/main/java/com/example/networkapp/viewmodel/PostViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.networkapp.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.networkapp.data.PostRepository
+
+class PostViewModelFactory(private val postRepository: PostRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PostViewModel::class.java)) {
+            return PostViewModel(postRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
